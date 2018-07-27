bbac1e9aa50f2ac7cee5f1983559894b8e315735
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 13:45:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,4 +6,12 @@ class Anagram
   def initialize(word)
     @word = word
   end
+
+  def match(words)
+    matches = []
+    words.each do |w|
+      if w.split("").sort == @word.split("").sort 
+        matches << w 
+      end 
+    end 
 end
